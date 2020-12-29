8591f3b5047e1140a2c84738f7bc5148ecdcb43a
adarshmalviyaaa <69356680+adarshmalviyaaa@users.noreply.github.com>
2020-12-29 10:23:55 +0530

    latest commit

---
+++ script1.js
@@ -0,0 +1,29 @@
+let img = document.querySelector('.abcdef');
+let btn1= document.querySelector('#btn1');
+let btn2= document.querySelector('#btn2');
+let btn3= document.querySelector('#btn3');
+let btn4= document.querySelector('#btn4');
+
+btn1.addEventListener('click',() => {
+    img.src = 'images/Web%201920%20%E2%80%93%202@2x.png';
+    
+    
+})
+
+btn2.addEventListener('click',() => {
+    img.src = 'images/Web%201920%20%E2%80%93%204@2x.png';
+    
+    
+})
+
+
+btn3.addEventListener('click',() => {
+     img.src='images/Web%201920%20%E2%80%93%205@2x.png' ;
+    
+    
+})
+btn4.addEventListener('click',() => {
+     img.src='images/Web%201920%20%E2%80%93%203@2x.png' ;
+    
+    
+})
